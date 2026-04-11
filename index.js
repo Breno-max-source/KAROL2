f403fe023338daa8a38fd22124ab1186049525fa
Breno-max-source <breno231xwn@gmail.com>
2026-04-10 23:34:55 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+}
+
+document.querySelector(".next").onclick = () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+};
+
+document.querySelector(".prev").onclick = () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+};
+
